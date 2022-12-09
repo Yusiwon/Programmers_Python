58cbef82d1d851f9ab3fce7350af2fa45181771a
Yusiwon <danseryu@naver.com>
2022-12-09 13:49:51 +0900

    Lv.0 점의 위치 구하기 [코딩테스트 입문] Day 10 : 조건문, 배열, 수학, 시뮬레이션

---
+++ src/연습문제/level0/점의위치구하기.py
@@ -0,0 +1,12 @@
+def solution(dot):
+    if dot[0] > 0 and dot[1] > 0:
+        return 1
+    elif dot[0] < 0 < dot[1]:
+        return 2
+    elif dot[0] < 0 and dot[1] < 0:
+        return 3
+    elif dot[0] > 0 > dot[1]:
+        return 4
+
+
+print(solution([-7, 9]))
