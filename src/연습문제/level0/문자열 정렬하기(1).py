9955f55c4a27f5d87670ceff49ce40462130a7b0
Yusiwon <danseryu@naver.com>
2022-12-10 21:23:15 +0900

    Lv.0 문자열 정렬하기(1) [코딩테스트 입문] Day 12 : 문자열, 배열, 사칙연산, 수학

---
+++ src/연습문제/level0/문자열 정렬하기(1).py
@@ -0,0 +1,13 @@
+def solution(my_string):
+    answer = []
+
+    num = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+    for s in my_string:
+        if s in num:
+            answer.append(int(s))
+
+    answer = sorted(answer)
+    return answer
+
+print(solution("hi12392"))
